b51a83e73a1d66e5ad56f2cd2d5ddbd20d5601c3
djmape <dheejaey07@gmail.com>
2024-05-11 18:31:44 +0800

    Added SQLite database start

---
+++ Planner/Models/Constants.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Planner.Models
+{
+    public static class Constants
+    {
+        public const string DatabaseFilename = "Planner.db";
+
+        public const SQLite.SQLiteOpenFlags Flags = SQLite.SQLiteOpenFlags.ReadWrite | SQLite.SQLiteOpenFlags.Create;
+
+        public static string DatabasePath => Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename);
+    }
+}
---
+++ Planner/Models/PlannerSQLiteDatabase.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+
+namespace Planner.Models
+{
+    internal class PlannerSQLiteDatabase
+    {
+        SQLiteAsyncConnection Database;
+
+        async Task Init()
+        {
+            if (Database is not null)
+                return;
+
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            var result = await Database.CreateTableAsync<Events>();
+        }
+    }
+}
